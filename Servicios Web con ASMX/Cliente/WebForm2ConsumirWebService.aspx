937bb6c7d03b9471569e49467509b97f049b9e75
chekulhan <46518731+chekulhan@users.noreply.github.com>
2019-01-16 12:06:38 +0100

    Add files via upload

---
+++ Servicios Web con ASMX/Cliente/WebForm2ConsumirWebService.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2ConsumirWebService.aspx.cs" Inherits="WebApplication1.WebForm2ConsumirWebService" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Numero 1: <asp:TextBox ID="txtNum1" runat="server"></asp:TextBox>
+            Numero 2: <asp:TextBox ID="txtNum2" runat="server"></asp:TextBox>
+
+            <asp:Label ID="lblResultado" runat="server" Text=""></asp:Label>
+            <asp:Button ID="btnGetWebServicio" runat="server" Text="GetWebServicio" OnClick="btnGetWebServicio_Click" />
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Servicios Web con ASMX/Cliente/WebForm2ConsumirWebService.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Serialization;
+
+namespace WebApplication1
+{
+    public partial class WebForm2ConsumirWebService : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGetWebServicio_Click(object sender, EventArgs e)
+        {
+            // Instanciar y llamar web servicio
+            MiPrimerServicioWeb w1 = new MiPrimerServicioWeb();
+
+            int Num1 = int.Parse(txtNum1.Text.ToString());
+            int Num2 = int.Parse(txtNum2.Text.ToString());
+
+            // Mostrar resultados
+            lblResultado.Text = w1.SumarNumeros(Num1, Num2).ToString();
+
+            
+        }
+
+       
+
+    }
+}
