79d7480efc3ba093b831658c6742c3aca182de3a
chekulhan <46518731+chekulhan@users.noreply.github.com>
2019-02-04 16:42:25 +0100

    Add files via upload

---
+++ MVC/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationMVC.Models; // Incluir referencia a Models
+
+
+
+namespace WebApplicationMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // PASO 1: Acceso a páginas
+        // GET: Home/Index
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        // GET: Home/Page2
+        public ActionResult Page2()
+        {
+            
+            return View();
+        }
+
+        //PASO 2: Mostrar los diferentes tipos de "ActionResult"
+        public ActionResult MostrarContent()
+        {
+            // Tipos de return types de MVC Controller (incluye View, Content, ReDirect, ...)
+            // https://www.c-sharpcorner.com/UploadFile/dacca2/various-return-types-from-mvc-controller/
+            return Content("<h1>Aqui tenemos contenido HTML </h1>");
+        }
+
+
+        // PASO 3: Mostrar la vista de datos
+        public ActionResult MostrarPaisesconSTRINGS()
+        {
+            List<string> listPaises = new List<string>() { "España", "Australia" }; // Crear una lista de strings
+
+            return View(listPaises); // Pasar la lista de paises a la vista a través de Modelo
+        }
+
+        // Paso 4: Pasar un modelo hecho de clases
+        public ActionResult MostrarLibros()
+        {
+            
+            List<Libro> Libros = new List<Libro>();
+
+            Libros.Add(new Libro("ASP.NET", "Microsoft"));
+            Libros.Add(new Libro("Java", "Java S.L."));
+
+
+            return View(Libros);  // Pasar objetos de libros
+        }
+
+
+
+       
+
+
+        // Paso 5: Pasar datos de Controlador a Vistas
+        public ActionResult UsarViewDatayViewBagyTempData()
+        {
+            
+            ViewData["Message"] = "Ejemplo de pasar datos con ViewData"; // Usar ViewData dictionary para pasar datos
+            ViewBag.TotalLibros = 5; // Usar viewBag para pasar datos
+            TempData["edad"] = 31; // Usar TempData
+
+            return View();
+        }
+
+       
+
+
+
+    }
+}
---
+++ MVC/Models/Libro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationMVC.Models
+{
+    
+    public class Libro
+    {
+        public string sTitulo { get; set; }
+        public string sAutor { get; set; }
+
+        public Libro(string sTitulo, string sAutor)
+        {
+            this.sTitulo = sTitulo;
+            this.sAutor = sAutor;
+        }
+    }
+}
