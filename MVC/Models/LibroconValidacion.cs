f868ec61459f7236d8306941c9d3055d8947f3d5
chekulhan <46518731+chekulhan@users.noreply.github.com>
2019-02-06 11:48:56 +0100

    Add files via upload

---
+++ MVC/Models/LibroconValidacion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations; //incluir DataAnnotations libreria
+
+namespace WebApplicationMVC.Models
+{
+    
+    public class LibroconValidacion
+    {
+        [StringLength(maximumLength:50,  ErrorMessage ="MAX 3 caracters" )]
+        [Required]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "Hace falta un Autor...")]
+        public string Autor { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        public decimal Precio { get; set; }
+        public LibroconValidacion(string sTitulo, string sAutor, decimal dPrecio)
+        {
+            this.Titulo = sTitulo;
+            this.Autor = sAutor;
+            this.Precio = dPrecio;
+        }
+
+        // Crear un constructor sin parámetros para evitar errores en EDITAR
+        public LibroconValidacion()
+        {
+
+        }
+    }
+}
