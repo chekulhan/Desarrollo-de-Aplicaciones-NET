8828034572b8ceab21412c2a47361ef24d67e3bc
chekulhan <46518731+chekulhan@users.noreply.github.com>
2019-02-06 16:32:19 +0100

    Add files via upload

---
+++ REST/Controllers/PersonaController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplicationREST.Models;
+
+
+
+namespace WebApplicationREST.Controllers
+{
+    public class PersonaController : ApiController
+    {
+
+        Persona[] p = new Persona[]
+       {
+            new Persona { ID = 1, Nombre = "Jon", Apellido = "Smith" },
+            new Persona { ID = 2, Nombre = "Axl", Apellido = "Rose" },
+            new Persona { ID = 3, Nombre = "Slash", Apellido = "AAAA" }
+       };
+
+        public IEnumerable<Persona> GetAllPersonas()
+        {
+            return p;
+        }
+
+        public IHttpActionResult GetPersona(int id)
+        {
+            var persona = p.FirstOrDefault((p) => p.ID == id);
+            if (persona == null)
+            {
+                return NotFound();
+            }
+            return Ok(persona);
+        }
+
+    }
+}
---
+++ REST/Models/Persona.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationREST.Models
+{
+    public class Persona
+    {
+
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        public DateTime FechadeNacimiento { get; set; }
+
+        public string Email { get; set; }
+        public Persona(int ID, string Nombre, string Apellido, string Email)
+        {
+            this.ID = ID;
+            this.Nombre = Nombre;
+            this.Apellido = Apellido;
+            this.Email = Email;
+        }
+
+        // Crear un constructor sin parámetros para evitar errores en EDITAR
+        public Persona()
+        {
+
+        }
+
+    }
+}
