e24baa7929210b82dfe89255ef68931195b1bf9e
chekulhan <46518731+chekulhan@users.noreply.github.com>
2019-02-19 12:55:14 +0100

    Add files via upload

---
+++ REST/Controllers/ProductosController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplicationMVCREST1.Models;
+
+
+namespace WebApplicationMVCREST1.Controllers
+{
+    public class ProductosController : ApiController
+    {
+        
+        Producto[] p = new Producto[] 
+        {
+            new Producto { ID=1, Nombre="PC", Precio=5},
+            new Producto { ID=2, Nombre="Tablet", Precio=6}
+        };
+
+        public IHttpActionResult GetProducto(int ID) {
+            var producto = p.FirstOrDefault((p) => p.ID == ID);
+            if (producto == null)
+            {
+                //return BadRequest(); OR NotFound(); Son helper methods de IHttpActionResult
+                return Content(HttpStatusCode.NotFound, "Producto No disponible");
+            }
+            return Ok(producto);
+        }
+
+        public IHttpActionResult GetProductos()
+        {
+            return Ok(p);
+        }
+
+
+        // pasar parameteros simples en QueryString con: public IHttpActionResult Post(int ID, string Nombre )
+        // cambiar los parámteros simples para HTTP BODY: public IHttpActionResult Post([FromBody]string Nombre)
+        public IHttpActionResult Post(Producto p)
+        {
+            
+            return Ok();
+        }
+
+
+    }
+}
