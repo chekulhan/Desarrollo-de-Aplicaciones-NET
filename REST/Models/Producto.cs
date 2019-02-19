620ccc9b21c00057f72cb432bee46df3464ada1c
chekulhan <46518731+chekulhan@users.noreply.github.com>
2019-02-19 12:54:53 +0100

    Add files via upload

---
+++ REST/Models/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationMVCREST1.Models
+{
+    public class Producto
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+        
+
+    }
+}
