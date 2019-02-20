bd1664b860df50d6bd90a110ee84b6e6fbbe4f84
chekulhan <46518731+chekulhan@users.noreply.github.com>
2019-02-20 14:10:25 +0100

    Add files via upload

--- REST/Controllers/ProductosController.cs
+++ REST/Controllers/ProductosController.cs
@@ -1,22 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
+
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using WebApplicationMVCREST1.Models;
-
+using System.Collections.Generic;
 
 namespace WebApplicationMVCREST1.Controllers
 {
     public class ProductosController : ApiController
     {
-        
-        Producto[] p = new Producto[] 
+
+
+
+        static List<Producto> p = new List<Producto>(
+                        new Producto[] {new Producto(1,"PC", 5),
+                       new Producto(2, "Tablet", 6) });
+
+
+        /*Producto[] p = new Producto[] 
         {
             new Producto { ID=1, Nombre="PC", Precio=5},
             new Producto { ID=2, Nombre="Tablet", Precio=6}
-        };
+        }; */
 
         public IHttpActionResult GetProducto(int ID) {
             var producto = p.FirstOrDefault((p) => p.ID == ID);
@@ -31,14 +38,16 @@ namespace WebApplicationMVCREST1.Controllers
         public IHttpActionResult GetProductos()
         {
             return Ok(p);
+            //return Json(p);
+ 
         }
 
 
         // pasar parameteros simples en QueryString con: public IHttpActionResult Post(int ID, string Nombre )
         // cambiar los parámteros simples para HTTP BODY: public IHttpActionResult Post([FromBody]string Nombre)
-        public IHttpActionResult Post(Producto p)
+        public IHttpActionResult Post(Producto producto)
         {
-            
+            p.Add(producto);
             return Ok();
         }
 
