5a34537208275c8ec3f1a86bc1b642169a202c86
chekulhan <46518731+chekulhan@users.noreply.github.com>
2019-02-22 11:44:04 +0100

    Add files via upload

---
+++ SQL Server/Windows Forms ADO.NET Basic 1/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtEmail);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtApellido);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtNombre);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtID);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(28, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(409, 149);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 235);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(551, 150);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnActualizar);
+            this.panel2.Controls.Add(this.btnBorrar);
+            this.panel2.Controls.Add(this.btnAdd);
+            this.panel2.Location = new System.Drawing.Point(468, 45);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(111, 149);
+            this.panel2.TabIndex = 2;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(15, 109);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 28);
+            this.btnActualizar.TabIndex = 2;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(15, 53);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(75, 28);
+            this.btnBorrar.TabIndex = 1;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(15, 19);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 28);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(61, 16);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 1;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(61, 53);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(61, 92);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Apellido";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(257, 16);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(216, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Email";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 394);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SQL Server/Windows Forms ADO.NET Basic 1/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+
+        private string connectionString = @"Data Source = XXXXXXXXXXXX";
+        private SqlConnection conn;
+        private SqlCommand cmd;
+
+
+        private void RefreshGrid()
+        {
+
+            cmd = new SqlCommand("USP_GetEmpleados", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+
+            // Bind datos al formulario
+            dataGridView1.DataSource = dt;
+            
+            dr.Close();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(connectionString); // Conectar a la base de datos
+            conn.Open();
+
+            RefreshGrid(); // actualizar los datos en el grid
+        }
+
+        private void Form1_FormClosed(Object sender, FormClosedEventArgs e)
+        {
+
+            Debug.Write("Closing");
+            conn.Close(); // Cerrrar conexion de base de datos
+
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int iID;
+            int iResult;
+
+            cmd = new SqlCommand("USP_InsertEmpleado", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@NombredeEmpleado", txtNombre.Text.ToString());
+            cmd.Parameters.AddWithValue("@Apellido", txtApellido.Text.ToString());
+            cmd.Parameters.AddWithValue("@Email", txtEmail.Text.ToString());
+
+            //Establecer parámetro de OUTPUT, en este caso en nuevo ID de SCOPE_IDENTITY()
+            SqlParameter parameterOUTPUT = new SqlParameter();
+            parameterOUTPUT.ParameterName = "@ID";
+            parameterOUTPUT.SqlDbType = SqlDbType.Int;
+            parameterOUTPUT.Direction = ParameterDirection.Output;
+            cmd.Parameters.Add(parameterOUTPUT);
+
+            //Establecer parámetro de RETORNO
+            SqlParameter parameterRETURN = new SqlParameter("@ReturnValue", System.Data.SqlDbType.Int);
+            parameterRETURN.Direction = System.Data.ParameterDirection.ReturnValue;
+            cmd.Parameters.Add(parameterRETURN);
+
+
+            cmd.ExecuteNonQuery();
+            iResult = (int)cmd.Parameters["@ReturnValue"].Value;
+
+            if (iResult ==0) { // Comprobar parçametro de RETORNO
+                iID = (int)cmd.Parameters["@ID"].Value; // Get New ID
+
+                txtID.Text = iID.ToString();
+                MessageBox.Show("Un nuevo empleados ha sido creado con ID " + iID.ToString());
+            }
+
+    }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            RefreshGrid(); // actualizar los datos en el grid
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int iID;
+            int rowIndex = e.RowIndex;
+            //DataGridViewRow row = dataGridView1.Rows[rowIndex];
+
+            iID = (int) dataGridView1.Rows[e.RowIndex].Cells[0].Value;
+
+
+            cmd = new SqlCommand("USP_GetEmpleado", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter parameterINPUT = new SqlParameter();
+            parameterINPUT.ParameterName = "@ID";
+            parameterINPUT.SqlDbType = SqlDbType.Int;
+            parameterINPUT.Direction = ParameterDirection.Input;
+            parameterINPUT.Value = iID;
+            cmd.Parameters.Add(parameterINPUT);
+
+            SqlDataReader sqlReader = cmd.ExecuteReader();
+
+            if (sqlReader.HasRows)
+            {
+                sqlReader.Read();
+                txtID.Text = sqlReader["IDEmpleado"].ToString();
+                txtNombre.Text = sqlReader["Nombre"].ToString();
+                txtApellido.Text = sqlReader["Apellido"].ToString();
+                txtEmail.Text = sqlReader["Email"].ToString();
+
+            }
+            sqlReader.Close();
+            
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+
+            int iResult;
+
+            cmd = new SqlCommand("USP_BorrarEmpleado", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("@ID", SqlDbType.Int);
+            cmd.Parameters["@ID"].Value = txtID.Text;
+
+            //Establecer parámetro de RETORNO
+            SqlParameter parameterRETURN = new SqlParameter("@ReturnValue", System.Data.SqlDbType.Int);
+            parameterRETURN.Direction = System.Data.ParameterDirection.ReturnValue;
+            cmd.Parameters.Add(parameterRETURN);
+
+            cmd.ExecuteNonQuery();
+
+            iResult = (int)cmd.Parameters["@ReturnValue"].Value;
+
+            if (iResult == 0)
+            { // Comprobar parçametro de RETORNO
+
+                MessageBox.Show("Un empleado ha sido borrado con ID " + txtID.Text.ToString());
+                //Resetear controles
+                txtID.Text = "";
+                txtNombre.Text = "";
+                txtApellido.Text = "";
+                txtEmail.Text = "";
+
+                RefreshGrid();
+            }
+            else
+            {
+                MessageBox.Show("Empleado no existe. ID " + txtID.Text.ToString());
+            }
+        }
+    }
+}
