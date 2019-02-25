17feef3bf81a6d39aa32db73b9de878224e9e837
chekulhan <46518731+chekulhan@users.noreply.github.com>
2019-02-25 09:17:28 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace WindowsFormsERPVuelos
+{
+    public partial class Form1 : Form
+    {
+
+        private string connectionString = @"Data Source = XXXXX";
+        private SqlConnection conn;
+        private SqlCommand cmd;
+
+
+        private void RefreshGrid()
+        {
+
+            cmd = new SqlCommand("USP_GetVuelos", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+
+            // Bind datos al formulario
+            dataGridView1.DataSource = dt;
+            
+            dr.Close();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(connectionString); // Conectar a la base de datos
+            conn.Open();
+
+            RefreshGrid(); // actualizar los datos en el grid
+        }
+
+        private void Form1_FormClosed(Object sender, FormClosedEventArgs e)
+        {
+
+            conn.Close(); // Cerrrar conexion de base de datos
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int iID;
+            int iResult;
+
+            cmd = new SqlCommand("USP_InsertarVuelo", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@NombreVuelo", txtVuelo.Text.ToString());
+            cmd.Parameters.AddWithValue("@Destino", txtDestino.Text.ToString());
+            cmd.Parameters.AddWithValue("@Avion", txtAvion.Text.ToString());
+
+            //Establecer parámetro de OUTPUT, en este caso en nuevo ID de SCOPE_IDENTITY()
+            SqlParameter parameterOUTPUT = new SqlParameter();
+            parameterOUTPUT.ParameterName = "@ID";
+            parameterOUTPUT.SqlDbType = SqlDbType.Int;
+            parameterOUTPUT.Direction = ParameterDirection.Output;
+            cmd.Parameters.Add(parameterOUTPUT);
+
+            //Establecer parámetro de RETORNO
+            SqlParameter parameterRETURN = new SqlParameter("@ReturnValue", System.Data.SqlDbType.Int);
+            parameterRETURN.Direction = System.Data.ParameterDirection.ReturnValue;
+            cmd.Parameters.Add(parameterRETURN);
+
+
+            cmd.ExecuteNonQuery();
+            iResult = (int)cmd.Parameters["@ReturnValue"].Value;
+
+            if (iResult ==0) { // Comprobar parámetro de RETORNO
+                iID = (int)cmd.Parameters["@ID"].Value; // Get New ID
+
+                txtID.Text = iID.ToString();
+                MessageBox.Show("Un nuevo vuelo ha sido creado con ID " + iID.ToString());
+            }
+
+    }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            RefreshGrid(); // actualizar los datos en el grid
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int iID;
+            int rowIndex = e.RowIndex;
+
+            iID = (int) dataGridView1.Rows[e.RowIndex].Cells[0].Value;
+
+
+            cmd = new SqlCommand("USP_GetVuelo", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter parameterINPUT = new SqlParameter();
+            parameterINPUT.ParameterName = "@ID";
+            parameterINPUT.SqlDbType = SqlDbType.Int;
+            parameterINPUT.Direction = ParameterDirection.Input;
+            parameterINPUT.Value = iID;
+            cmd.Parameters.Add(parameterINPUT);
+
+            SqlDataReader sqlReader = cmd.ExecuteReader();
+
+            if (sqlReader.HasRows)
+            {
+                sqlReader.Read();
+                txtID.Text = sqlReader["ID"].ToString();
+                txtVuelo.Text = sqlReader["Nombre"].ToString();
+                txtDestino.Text = sqlReader["Destino"].ToString();
+                txtAvion.Text = sqlReader["Avion"].ToString();
+
+            }
+            sqlReader.Close();
+            
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+
+            int iResult;
+
+            cmd = new SqlCommand("USP_BorrarVuelo", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("@ID", SqlDbType.Int);
+            cmd.Parameters["@ID"].Value = txtID.Text;
+
+            //Establecer parámetro de RETORNO
+            SqlParameter parameterRETURN = new SqlParameter("@ReturnValue", System.Data.SqlDbType.Int);
+            parameterRETURN.Direction = System.Data.ParameterDirection.ReturnValue;
+            cmd.Parameters.Add(parameterRETURN);
+
+            cmd.ExecuteNonQuery();
+
+            iResult = (int)cmd.Parameters["@ReturnValue"].Value;
+
+            if (iResult == 0)
+            { // Comprobar parçametro de RETORNO
+
+                MessageBox.Show("Un vuelo ha sido borrado con ID " + txtID.Text.ToString());
+                //Resetear controles
+                txtID.Text = "";
+                txtVuelo.Text = "";
+                txtDestino.Text = "";
+                txtAvion.Text = "";
+
+                RefreshGrid();
+            }
+            else
+            {
+                MessageBox.Show("Vuelo no existe. ID " + txtID.Text.ToString());
+            }
+        }
+
+        
+    }
+}
---
+++ Form1.designer.cs
@@ -0,0 +1,264 @@
+﻿namespace WindowsFormsERPVuelos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAvion = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDestino = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtVuelo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txtAvion);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtDestino);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtVuelo);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtID);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(28, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(409, 149);
+            this.panel1.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(259, 49);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(108, 17);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "LH 623";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(259, 82);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 17);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Bilbao, Madrid";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(259, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 28);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Avión Comercial, de Carga o Militar";
+            // 
+            // txtAvion
+            // 
+            this.txtAvion.Location = new System.Drawing.Point(132, 109);
+            this.txtAvion.Name = "txtAvion";
+            this.txtAvion.Size = new System.Drawing.Size(100, 20);
+            this.txtAvion.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Tipo de Avión";
+            // 
+            // txtDestino
+            // 
+            this.txtDestino.Location = new System.Drawing.Point(132, 79);
+            this.txtDestino.Name = "txtDestino";
+            this.txtDestino.Size = new System.Drawing.Size(100, 20);
+            this.txtDestino.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Destino";
+            // 
+            // txtVuelo
+            // 
+            this.txtVuelo.Location = new System.Drawing.Point(132, 46);
+            this.txtVuelo.Name = "txtVuelo";
+            this.txtVuelo.Size = new System.Drawing.Size(100, 20);
+            this.txtVuelo.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Número de Vuelo";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(132, 16);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vuelo ID";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 214);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(551, 171);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnActualizar);
+            this.panel2.Controls.Add(this.btnBorrar);
+            this.panel2.Controls.Add(this.btnAdd);
+            this.panel2.Location = new System.Drawing.Point(468, 45);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(111, 149);
+            this.panel2.TabIndex = 2;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(15, 109);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 28);
+            this.btnActualizar.TabIndex = 2;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(15, 53);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(75, 28);
+            this.btnBorrar.TabIndex = 1;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(15, 19);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 28);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Insertar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(28, 13);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(238, 26);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "ERP Módulo de Vuelos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 394);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDestino;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtVuelo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAvion;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
