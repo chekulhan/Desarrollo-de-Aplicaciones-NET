319fadf3e35797fe5253cc639991ad5d31658d32
chekulhan <46518731+chekulhan@users.noreply.github.com>
2019-02-26 13:08:56 +0100

    Add files via upload

---
+++ SQL Server/XML/XML.sql
@@ -0,0 +1,6 @@
+DECLARE @x xml  
+SET @x = '<?xml version="1.0" ?><selva><animal id="1"><nombre>Werthers</nombre><tipo>Pantera</tipo><color>Negro</color><edad>12</edad></animal><animal id="2"><nombre>Bun</nombre><tipo>León</tipo><color>Marrón</color><edad>15</edad> </animal></selva>'
+
+
+
+
