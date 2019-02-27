37b19b757865298f455c327765a9bb73c71ca0e3
chekulhan <46518731+chekulhan@users.noreply.github.com>
2019-02-27 12:37:45 +0100

    Add files via upload

--- SQL Server/Funciones/EjerciciosFunciones.sql
+++ SQL Server/Funciones/EjerciciosFunciones.sql
@@ -104,3 +104,36 @@ BEGIN
 
 END
 GO
+
+
+
+
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date, ,>
+-- Description:	<Description, ,>
+-- =============================================
+CREATE FUNCTION [dbo].[fct_CalcularDescuento]
+(
+	-- Add the parameters for the function here
+	@Precio money
+)
+RETURNS money
+AS
+BEGIN
+	-- Declare the return variable here
+	DECLARE @Result money
+
+	-- Add the T-SQL statements to compute the return value here
+	SELECT @Result = @Precio -1
+
+	-- Return the result of the function
+	RETURN @Result
+
+END
+GO
+
