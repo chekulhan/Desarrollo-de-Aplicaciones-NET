c928d071f34438147014a8dc39ce9f2fe03fcdce
chekulhan <46518731+chekulhan@users.noreply.github.com>
2019-03-12 12:55:50 +0100

    Add files via upload

---
+++ SQL Server/Errores/WebForm1.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+        </div>
+        <asp:Button ID="Button1" runat="server" Text="Button" onclick="btnDatos_Click"/>
+       
+    </form>
+</body>
+</html>
---
+++ SQL Server/Errores/WebForm1.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnDatos_Click(object sender, EventArgs e)
+        {
+
+            SqlDataReader dr;
+            DataTable dt;
+
+            string connectionString = ConfigurationManager.ConnectionStrings["DBConexion"].ToString();
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = connectionString;
+
+            // Ejecutamos un procedimiento almacenado que llama a RAISERROR ('Error en Transact SQL.', 16, 1 ) sin TRY..CATCH
+            SqlCommand cmd = new SqlCommand("usp_RaiseError", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            conn.Open();
+
+            try
+            {
+                
+                dr = cmd.ExecuteReader();
+
+                // Bind datos al formulario
+                GridView1.DataSource = dr;
+                GridView1.DataBind();
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Error");
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+               
+                if (conn != null) conn.Close();  
+            }
+
+        }
+
+        
+    }
+}
