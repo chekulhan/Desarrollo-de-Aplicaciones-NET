e843946e6d53b263f257e975fdd9fd408bd3c30d
chekulhan <46518731+chekulhan@users.noreply.github.com>
2019-03-20 13:13:38 +0100

    Add files via upload

---
+++ OdooPostgresSQL/Controllers/ProductosController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using System.Data;
+using Npgsql;
+using NpgsqlTypes;
+
+
+namespace OdooPostgresSQL.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            string sContent;
+            string connStr = ConfigurationManager.ConnectionStrings["OdooPostgres"].ConnectionString;
+
+            NpgsqlConnection conn = new NpgsqlConnection(connStr);
+            conn.Open();
+
+            NpgsqlCommand cmd = new NpgsqlCommand();
+            cmd.CommandText = "SELECT * FROM product_template;";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = conn;
+
+            NpgsqlDataReader dr;
+            dr = cmd.ExecuteReader();
+            sContent = "OK";
+
+            while (dr.Read()) { 
+                sContent = sContent + dr["id"].ToString() + " " + dr["name"].ToString() + "<br>";
+            }
+
+
+            dr.Close();
+            conn.Close();
+
+            return Content(sContent);
+
+            //return View();
+        }
+    }
+}
