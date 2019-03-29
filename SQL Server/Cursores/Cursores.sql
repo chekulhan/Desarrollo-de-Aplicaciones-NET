4646acdc51dac1af6776dc7f96ac43a1495a42a3
chekulhanseim <48593404+chekulhanseim@users.noreply.github.com>
2019-03-29 12:52:22 +0100

    Update Cursores.sql

--- SQL Server/Cursores/Cursores.sql
+++ SQL Server/Cursores/Cursores.sql
@@ -86,3 +86,38 @@ WHILE @@FETCH_STATUS = 0
 
 CLOSE C
 DEALLOCATE C
+
+
+
+
+
+-- Actualizar datos en tabla a traves de CURSOR
+DECLARE @id int
+DECLARE @nombre varchar(200)
+DECLARE @descripcion varchar(150)
+DECLARE @product_status varchar(100)
+DECLARE @listprice money
+
+
+DECLARE cursorProductos CURSOR FOR
+SELECT Product_ID, PRODUCT_NAME, PRODUCT_DESCRIPTION, PRODUCT_STATUS, LIST_PRICE FROM [oe].[PRODUCT_INFORMATION]
+WHERE PRODUCT_STATUS = 'under development'
+
+SELECT * FROM sys.dm_exec_cursors(@@spid);
+
+
+OPEN cursorProductos
+FETCH cursorProductos INTO @id, @nombre, @descripcion,@product_status,@listprice
+
+WHILE (@@fetch_status=0)
+	BEGIN
+		
+			print  'Datos de productos aqui... ' + @nombre 
+			UPDATE [oe].[PRODUCT_INFORMATION]
+			SET LIST_PRICE = LIST_PRICE + 1
+			WHERE CURRENT OF cursorProductos
+		FETCH cursorProductos INTO @id, @nombre, @descripcion,@product_status,@listprice
+	END
+
+CLOSE cursorProductos
+DEALLOCATE cursorProductos
