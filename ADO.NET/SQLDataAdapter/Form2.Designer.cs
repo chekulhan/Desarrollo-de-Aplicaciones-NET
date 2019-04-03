955bd0dd43302422324280c490621cbebd97bc09
chekulhanseim <48593404+chekulhanseim@users.noreply.github.com>
2019-04-03 12:43:39 +0200

    Add files via upload

---
+++ ADO.NET/SQLDataAdapter/Form2.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WindowsFormsERPVuelos
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridEmpleados = new System.Windows.Forms.DataGridView();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.btnComandos = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridEmpleados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridEmpleados
+            // 
+            this.gridEmpleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridEmpleados.Location = new System.Drawing.Point(12, 101);
+            this.gridEmpleados.Name = "gridEmpleados";
+            this.gridEmpleados.Size = new System.Drawing.Size(529, 150);
+            this.gridEmpleados.TabIndex = 0;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(401, 281);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(125, 36);
+            this.btnUpdate.TabIndex = 1;
+            this.btnUpdate.Text = "Actualizar";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(234, 281);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(124, 36);
+            this.btnInsert.TabIndex = 2;
+            this.btnInsert.Text = "Ejemplo de Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.Location = new System.Drawing.Point(110, 281);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(105, 36);
+            this.btnInfo.TabIndex = 3;
+            this.btnInfo.Text = "Ver más info";
+            this.btnInfo.UseVisualStyleBackColor = true;
+            this.btnInfo.Click += new System.EventHandler(this.btnInfo_Click);
+            // 
+            // btnComandos
+            // 
+            this.btnComandos.Location = new System.Drawing.Point(13, 281);
+            this.btnComandos.Name = "btnComandos";
+            this.btnComandos.Size = new System.Drawing.Size(91, 36);
+            this.btnComandos.TabIndex = 4;
+            this.btnComandos.Text = "Usar Comandos";
+            this.btnComandos.UseVisualStyleBackColor = true;
+            this.btnComandos.Click += new System.EventHandler(this.btnComandos_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 339);
+            this.Controls.Add(this.btnComandos);
+            this.Controls.Add(this.btnInfo);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.gridEmpleados);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridEmpleados)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridEmpleados;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnInfo;
+        private System.Windows.Forms.Button btnComandos;
+    }
+}
---
+++ ADO.NET/SQLDataAdapter/Form2.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+
+
+namespace WindowsFormsERPVuelos
+{
+    public partial class Form2 : Form
+    {
+
+        private string connectionString = @"Data Source = XXXXXXXX";
+        private SqlConnection conn;
+        private DataSet empleadosDS;
+        SqlDataAdapter adapter;
+
+
+        private void RefreshGrid()
+        {
+
+            conn = new SqlConnection(connectionString); // Fijáte que la conexion NO esta ABIERTA
+
+
+            adapter.Fill(empleadosDS, "Empleados"); // al ejecutar Fill(), se abre, vuelca los datos y se cierra la conexión 
+         
+            gridEmpleados.DataSource = empleadosDS.Tables["Empleados"];
+
+        }
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(connectionString); // Conectar a la base de datos
+            string queryString = "SELECT * FROM dbo.Empleados";
+            adapter = new SqlDataAdapter(queryString, conn);
+            empleadosDS = new DataSet();
+            RefreshGrid();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+            adapter.Update(empleadosDS, "Empleados"); // Fijáte que la conexion NO esta ABIERTA
+
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            // Ejemplo básico de un INSERT sin usar el GridView
+            // Se puede crear TextBoxes con valores para insertar en una nueva fila de DataTable
+
+            DataTable table = empleadosDS.Tables["Empleados"];
+            DataRow rowData;
+            rowData = table.NewRow();
+            rowData["Nombre"] = "nuevo Nombre"; // valores que se puede coger de Textbox
+            rowData["Apellido"] = "nuevo Apellido";
+            table.Rows.Add(rowData);
+
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+            adapter.Update(empleadosDS, "Empleados"); // Fijáte que la conexion NO esta ABIERTA
+
+
+            RefreshGrid();
+        }
+
+        private void btnInfo_Click(object sender, EventArgs e)
+        {
+            // Mostrar información sobre RowState (de cada fila) y DataRowVersion (de cada columna)
+            DataTable table = empleadosDS.Tables["Empleados"];
+            foreach (DataRow dr in table.Rows)
+            {
+                Debug.Print("ROWSTATE: " + dr.RowState.ToString()); //Unchanged, Modified, ...
+                Debug.Print("Original: " + dr["Nombre", DataRowVersion.Original].ToString());
+                Debug.Print("Nuevo valor: " + dr["Nombre", DataRowVersion.Current].ToString());
+
+            }
+        
+        }
+
+        private void btnComandos_Click(object sender, EventArgs e)
+        {
+           
+
+            // Ver las consultas que crea automaticamente el SQLCommandBuilder
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+            SqlCommand comm = builder.GetUpdateCommand();
+            Debug.Print(comm.CommandText.ToString());
+
+
+            // Construir el update command, diferente de lo que nos proporciona el SqlCommandBuilder
+            // en este caso, solo estoy actualizando la columna Nombre, y nada mas....
+
+            string sqlText = "Update Empleados SET Nombre = @Nombre WHERE IdEmpleado=@IdOriginal";
+            SqlCommand updateCommand = new SqlCommand(sqlText, conn);
+            updateCommand.Parameters.Add("@Nombre", SqlDbType.NVarChar, 50, "Nombre"); // Nombre es la fuente de Columna 
+            updateCommand.Parameters.Add("@IdOriginal", SqlDbType.Int, 0, "IdEmpleado");
+            updateCommand.Parameters["@IdOriginal"].SourceVersion =DataRowVersion.Original;
+            adapter.UpdateCommand = updateCommand;
+
+            adapter.Update(empleadosDS, "Empleados");
+
+
+            RefreshGrid(); // Refrescar grid
+
+        }
+    }
+}
