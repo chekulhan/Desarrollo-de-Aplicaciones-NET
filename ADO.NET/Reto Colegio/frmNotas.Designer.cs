880131da2d39a292085e2fe005e801a7002b29fc
chekulhanseim <48593404+chekulhanseim@users.noreply.github.com>
2019-04-09 11:53:58 +0200

    Add files via upload

---
+++ ADO.NET/Reto Colegio/frmNotas.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsERPVuelos
+{
+    partial class frmNotas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgridNotas = new System.Windows.Forms.DataGridView();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnActualizarconTransaccion = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgridNotas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgridNotas
+            // 
+            this.dgridNotas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgridNotas.Location = new System.Drawing.Point(33, 69);
+            this.dgridNotas.Name = "dgridNotas";
+            this.dgridNotas.Size = new System.Drawing.Size(349, 177);
+            this.dgridNotas.TabIndex = 0;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(388, 208);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(86, 38);
+            this.btnActualizar.TabIndex = 1;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnActualizarconTransaccion
+            // 
+            this.btnActualizarconTransaccion.Location = new System.Drawing.Point(494, 208);
+            this.btnActualizarconTransaccion.Name = "btnActualizarconTransaccion";
+            this.btnActualizarconTransaccion.Size = new System.Drawing.Size(104, 38);
+            this.btnActualizarconTransaccion.TabIndex = 2;
+            this.btnActualizarconTransaccion.Text = "Actualizar con Transacciones";
+            this.btnActualizarconTransaccion.UseVisualStyleBackColor = true;
+            this.btnActualizarconTransaccion.Click += new System.EventHandler(this.btnActualizarconTransaccion_Click);
+            // 
+            // frmNotas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 345);
+            this.Controls.Add(this.btnActualizarconTransaccion);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.dgridNotas);
+            this.Name = "frmNotas";
+            this.Text = "frmNotas";
+            this.Load += new System.EventHandler(this.frmNotas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgridNotas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgridNotas;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnActualizarconTransaccion;
+    }
+}
---
+++ ADO.NET/Reto Colegio/frmNotas.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Data.SqlClient;
+
+namespace WindowsFormsERPVuelos
+{
+    public partial class frmNotas : Form
+    {
+
+        private string connectionString = @"Data Source = XXXXXX";
+        private SqlConnection conn;
+        private DataSet notasDS;
+        SqlDataAdapter adapter;
+        SqlTransaction tran;
+
+        public frmNotas()
+        {
+            InitializeComponent();
+        }
+
+        private void frmNotas_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(connectionString); // Conectar a la base de datos
+            string queryString = "SELECT IDAlumno, (SELECT nombre + ' ' + apellido FROM cole.alumnos AS a WHERE a.id = n.IDALUMNO) AS Nombre, Nota, IDESPECIALIDAD, CODCURSO, CODASIG  FROM cole.Notas AS n WHERE IDESPECIALIDAD = @IDEspecialidad AND CODCURSO = 1 AND CODASIG = '0369'";
+
+            adapter = new SqlDataAdapter(queryString, conn);
+
+            adapter.SelectCommand.Parameters.AddWithValue("@IDEspecialidad", "ASIR"); // conseguir valor de SelectedValue() de ComboBoxes
+            // Añadir más parametro aqui para la consulta...
+
+            notasDS = new DataSet();
+            adapter.Fill(notasDS, "Notas"); // al ejecutar Fill(), se abre, vuelca los datos y se cierra la conexión 
+
+            dgridNotas.DataSource = notasDS.Tables["Notas"];
+
+            // Hacer invisible algunas columnas
+            dgridNotas.Columns["IDAlumno"].Visible = false; 
+            dgridNotas.Columns["IDESPECIALIDAD"].Visible = false;
+            dgridNotas.Columns["CODCURSO"].Visible = false;
+            dgridNotas.Columns["CODASIG"].Visible = false;
+
+            dgridNotas.Columns["Nombre"].ReadOnly = true; // No se puede cambiar los datos del Nombre del Alumno
+
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+            adapter.Update(notasDS.Tables["Notas"]);
+        }
+
+        private void btnActualizarconTransaccion_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+
+                SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+                // Fijate en el orden de cosas ... hace falta pedir comandos del builder antes de asignar una transaccion
+                builder.GetDeleteCommand();
+                builder.GetUpdateCommand();
+                builder.GetInsertCommand();
+
+                conn.Open();
+                tran = conn.BeginTransaction();
+
+                builder.GetDeleteCommand().Transaction = tran;
+                builder.GetUpdateCommand().Transaction = tran;
+                builder.GetInsertCommand().Transaction = tran;
+
+                adapter.Update(notasDS.Tables["Notas"]);
+
+                tran.Commit();
+            }
+            catch (Exception ex)
+            {
+                Debug.Print(ex.Message);
+                tran.Rollback();
+            }
+            finally
+            {
+                conn.Close();
+            }
+           
+        }
+    }
+}
