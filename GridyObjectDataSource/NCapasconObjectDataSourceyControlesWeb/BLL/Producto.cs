ef40c69f5ad0566417d307472d36d7b777ac5a74
chekulhanseim <48593404+chekulhanseim@users.noreply.github.com>
2019-05-08 12:00:08 +0200

    Add files via upload

--- GridyObjectDataSource/NCapasconObjectDataSourceyControlesWeb/BLL/Producto.cs
+++ GridyObjectDataSource/NCapasconObjectDataSourceyControlesWeb/BLL/Producto.cs
@@ -7,10 +7,24 @@ namespace WebNCapas.BLL
 {
     public class Producto
     {
+        private string nombre;
+        
+
         public int ID { get; set; }
-        public string Nombre { get; set; }
+        public string Nombre
+        {
+            get {  return nombre.ToUpper();  }
+            set { nombre = value; }    
+        }
+
         public string Detalles { get; set; }
+        public string NombreyDetalles
+        {
+            get { return Nombre + " - " + Detalles; }
+        }
         public double Precio { get; set; }
         public string Foto { get; set; }
+        public bool Disponible { get; set; }
+            
     }
 }
--- GridyObjectDataSource/NCapasconObjectDataSourceyControlesWeb/BLL/ProductoMgr.cs
+++ GridyObjectDataSource/NCapasconObjectDataSourceyControlesWeb/BLL/ProductoMgr.cs
@@ -7,13 +7,34 @@ namespace WebNCapas.BLL
 {
     public class ProductoMgr
     {
+
+        public bool UpdateProducto(Producto p)
+        {
+
+            // Actualizar producto
+            return true;
+        }
+
+        public bool DeleteProducto(int ID)
+        {
+
+            // borrar producto
+            return true;
+        }
+
+        public bool AddProducto(Producto p)
+        {
+
+            // insertar producto
+            return true;
+        }
         public List<Producto> GetProductos()
         {
             List<Producto> listProductos = new List<Producto>
             {
-                new Producto {ID=1,  Nombre="Lenova", Detalles="Descricion de Lenova", Precio=250, Foto="images/laptop1.jpg"},
-                new Producto {ID=2,   Nombre="ACER", Detalles="Descricion de ACER", Precio=40, Foto="images/laptop2.jpg"},
-                new Producto { ID=3,  Nombre="Apple", Detalles="Descricion de Apple", Precio=150, Foto="images/laptop3.jpg"},
+                new Producto {ID=1,  Nombre="Lenova", Detalles="Descricion de Lenova", Precio=250, Foto="images/laptop1.jpg", Disponible=true},
+                new Producto {ID=2,   Nombre="ACER", Detalles="Descricion de ACER", Precio=40, Foto="images/laptop2.jpg", Disponible=true},
+                new Producto { ID=3,  Nombre="Apple", Detalles="Descricion de Apple", Precio=150, Foto="images/laptop3.jpg", Disponible=false},
             };
 
 
