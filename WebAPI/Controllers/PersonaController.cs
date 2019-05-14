5cc1ba7e926733ad989d575b12c4420c16a01878
chekulhanseim <48593404+chekulhanseim@users.noreply.github.com>
2019-05-14 13:44:39 +0200

    Add files via upload

---
+++ WebAPI/Controllers/PersonaController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class PersonaController : ApiController
+    {
+        Persona[] p = new Persona[]
+       {
+            new Persona { ID = 1, Nombre = "Jon", Apellido = "Smith" },
+            new Persona { ID = 2, Nombre = "Axl", Apellido = "Rose" },
+            new Persona { ID = 3, Nombre = "Slash", Apellido = "AAAA" }
+       };
+
+        public IHttpActionResult GetAllPersonas()
+        {
+            return Ok(p);
+        }
+
+        public IHttpActionResult GetPersona(int id)
+        {
+            var persona = p.FirstOrDefault((p) => p.ID == id);
+            if (persona == null)
+            {
+                return Content(HttpStatusCode.NotFound, "Persona No disponible");
+            }
+            return Ok(persona);
+        }
+
+
+    }
+}
---
+++ WebAPI/Models/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Persona
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+    }
+}
