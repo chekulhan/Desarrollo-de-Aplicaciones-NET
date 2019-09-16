8b923bfb616a0088286e21f7c28518689184749e
chekulhanseim <48593404+chekulhanseim@users.noreply.github.com>
2019-09-16 10:00:50 +0200

    Add files via upload

---
+++ Ficheros/Ficheros.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Ficheros
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(12, 12);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(192, 20);
+            this.txtFile.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(210, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Seleccionar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(12, 41);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(302, 155);
+            this.txtContent.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 228);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtFile);
+            this.Name = "Form1";
+            this.Text = "Ficheros";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtContent;
+    }
+}
+
---
+++ Ficheros/Ficheros.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Ficheros : Form
+    {
+        public Ficheros()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            string linea;
+
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter= "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                txtFile.Text = file.FileName;
+
+                using (StreamReader sr = new StreamReader(txtFile.Text))
+                {
+                    while ((linea = sr.ReadLine()) != null)
+                    {
+                        txtContent.AppendText(linea);
+                        txtContent.AppendText("\n");
+                    }
+
+                }
+            }
+
+        }
+
+
+       
+    }
+}
