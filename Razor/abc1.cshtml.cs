1d6ac4b3ed2b18690e78b12d11286e3fba32d932
chekulhanseim <48593404+chekulhanseim@users.noreply.github.com>
2020-01-15 12:31:59 +0100

    OnGet con parámetros
    Página para demostrar el uso de "asp-page-handler" para OnGet y "asp-route-carid" para pasar un parámetro
    asp-page-handler="CarOnly" coincide con el método OnGetCarOnly(int carid) con un parámetro

---
+++ Razor/abc1.cshtml.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using webapp1.Models;
+
+namespace webapp1.Pages
+{
+    public class abc1Model : PageModel
+    {
+        public string Message {get;set;}
+        public List<Car> Cars {get;set;}
+        private List<Car> GetCars() {
+
+           List<Car> cars = new List<Car>{
+                new Car{Id = 1, Make="Audi",Model="R8",Year=2014,Doors=2,Colour="Red",Price=79995},
+                new Car{Id = 2, Make="Aston Martin",Model="Rapide",Year=2010,Doors=2,Colour="Black",Price=54995},
+                new Car{Id = 3, Make="Porsche",Model=" 911 991",Year=2016,Doors=2,Colour="White",Price=155000},
+                new Car{Id = 4, Make="Mercedes-Benz",Model="GLE 63S",Year=2017,Doors=5,Colour="Blue",Price=83995},
+                new Car{Id = 5, Make="BMW",Model="X6 M",Year=2016,Doors=5,Colour="Silver",Price=62995},
+            };
+            return cars;
+        }
+        public void OnGet()
+        {
+            Cars = GetCars();
+            Message = "OnGet" ;
+        }
+
+        public void OnGetCarOnly(int carid)
+        {
+            Cars = GetCars();
+            // OJO hay que asignar la lista otra vez. O sea, este linea no functiona: Cars.Where(c=>c.Id==carid).ToList();
+            Cars = Cars.Where(c=>c.Id==carid).ToList();
+            Message = "OnGet Car Only:" + carid.ToString() + "CARS:" + Cars.Count();
+        }
+
+       
+       
+    }
+}
